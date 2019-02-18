37ec65c0b6b140a4277265d469b66f74e3acb0f5
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-18 16:25:01 -0500

    completes binary_search(), stretch

--- project/searching.py
+++ project/searching.py
@@ -7,16 +7,33 @@ def linear_search(arr, target):
       return index
   return -1   # not found
 
-# STRETCH: write an iterative implementation of Binary Search 
+# STRETCH: write an iterative implementation of Binary Search
+# Compare the item in the middle of the data set to the item we are searching for.
+#   If it is the same, stop. We found it and are done!
+#   Else, if the item we are searching for is LESS than the item in the middle:
+#     Eliminate the RHS of list. Repeat step 1 with only the LHS of list.
+#   Else, the item we are searching for is GREATER than the item in the middle:
+#     Eliminate the LHS of list. Repeat step 1 with only the RHS of the list.
+
 def binary_search(arr, target):
 
   if len(arr) == 0:
     return -1 # array empty
     
   low = 0
-  high = len(arr)-1
+  mid = (len(arr) - 1) // 2
+  high = len(arr) - 1
 
   # TO-DO: add missing code
+  while high != low:
+    if arr[mid] > target:
+      high = mid
+      mid = (high - low) // 2
+    elif arr[mid] < target:
+      low = mid
+      mid = (high - low) // 2
+    else:
+      return mid
 
   return -1 # not found
 
